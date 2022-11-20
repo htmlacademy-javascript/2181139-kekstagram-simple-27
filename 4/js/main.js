3a1d8168d491c3ec5d85b681ffcb7ba9f4b84d82
Keks <keks@htmlacademy.ru>
2022-11-21 00:31:09 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,5 @@
+import {generatePostedPhotos} from './data';
+import {randomInteger, isValidString} from './utils';
+generatePostedPhotos(25);
+randomInteger(10, 5);
+isValidString('World is beautiful', 50);
